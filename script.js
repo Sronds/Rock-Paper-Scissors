8693ae47ed5f7c9d7c51cc6f6af5d01bf99a0593
Youssef Srondy <srondyyoussef@gmail.com>
2022-04-19 11:12:57 +0200

    redid rock paper scissors (not done)

--- script.js
+++ script.js
@@ -1,55 +1,142 @@
-const choices = ["ROCK", "PAPER", "SCISSORS"];
+const body = document.body;
 
-function computerPlay() { //picks a random value from array
-    return choices[Math.floor(Math.random()*choices.length)];
+const readyToPlay = document.createElement("h1")
+readyToPlay.innerText = "Ready to Play?" 
+body.append(readyToPlay)
+
+const readyToPlay2 = document.createElement("h2")
+readyToPlay2.innerText = "Make your choice below." 
+body.append(readyToPlay2)
+
+const rock = document.createElement("button");
+rock.innerText = "Rock"
+rock.setAttribute("id", "rock")
+body.append(rock);
+
+const paper = document.createElement("button");
+paper.innerText = "Paper"
+paper.setAttribute("id", "paper")
+body.append(paper);
+
+const scissors = document.createElement("button");
+scissors.innerText = "Scissors"
+scissors.setAttribute("id", "scissors")
+body.append(scissors);
+
+const yourChoice = document.createElement("h3")
+yourChoice.innerText = "Your choice: " 
+body.append(yourChoice);
+
+const computerDecision = document.createElement("h3")
+computerDecision.innerText = "Computer choice: " 
+body.append(computerDecision);
+
+const decision = document.createElement("h2")
+decision.innerText = "";
+body.append(decision);
+
+let playerPoints = 0;
+let computerPoints = 0;
+
+let playerScore = document.createElement("h4")
+playerScore.innerText = "Player Score: " + playerPoints
+body.append(playerScore);
+
+
+let computerScore = document.createElement("h4")
+computerScore.innerText = "Computer Score: " + computerPoints
+body.append(computerScore);
+
+rock.addEventListener("click", choseRock);
+paper.addEventListener("click", chosePaper);
+scissors.addEventListener("click", choseScissors);
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+
+function choseRock() {
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    if (computerChoice == "Rock") {
+        yourChoice.innerText = "Your choice: Rock " 
+        computerDecision.innerText = "Computer chose: " + computerChoice
+        decision.innerText = "It's a tie! Play again"
+    } else if (computerChoice == "Paper") {
+        yourChoice.innerText = "Your choice: Rock " 
+        computerDecision.innerText = "Computer chose: " + computerChoice
+        decision.innerText = "You lose."
+        ++computerPoints
+        computerScore.innerText = "Computer Score: " + computerPoints;
+    } else if (computerChoice == "Scissors") {
+        yourChoice.innerText = "Your choice: Rock " 
+        computerDecision.innerText = "Computer chose: " + computerChoice
+        decision.innerText = "You win."
+        ++playerPoints
+        playerScore.innerText = "Player Score: " + playerPoints;
+    }
 }
 
-let scorePlayer = 0;
-let scoreComputer = 0;
-
-function oneRound() {
-    let playerChoice = prompt("Rock, Paper, Scissors?").toUpperCase()
-    let computerChoice = computerPlay();
-
-    if (playerChoice === computerChoice) {
-        return alert("It's a tie! Play again.")
-    } 
-    else if (playerChoice === "ROCK" && computerChoice === "PAPER") {
-        console.log(++scoreComputer);
-        return alert("You lose! Paper beats Rock.")
-    }
-    else if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
-        console.log(++scorePlayer)
-        return alert("You win! Rock beats Scissors.")
-    }
-    else if (playerChoice === "PAPER" && computerChoice === "ROCK") {
-        console.log(++scorePlayer)
-        return alert("You win! Paper beats Rock.")
-    }
-    else if (playerChoice === "PAPER" && computerChoice === "SCISSORS") {
-        console.log(++scoreComputer)
-        return alert("You lose! Scissors beats Paper.")
-    }
-    else if (playerChoice === "SCISSORS" && computerChoice === "ROCK") {
-        console.log(++scoreComputer)
-        return alert("You lose! Rock beats Scissors.")
-    }
-    else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
-        console.log(++scorePlayer)
-        return alert("You win! Scissors beats Paper.")
+function chosePaper() {
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    if (computerChoice == "Paper") {
+        yourChoice.innerText = "Your choice: Paper" 
+        computerDecision.innerText = "Computer choice: " + computerChoice
+        decision.innerText = "It's a tie! Play again"
+    } else if (computerChoice == "Rock") {
+        yourChoice.innerText = "Your choice: Paper " 
+        computerDecision.innerText = "Computer choice: " + computerChoice
+        decision.innerText = "You win."
+        ++playerPoints 
+        playerScore.innerText = "Player Score: " + playerPoints;
+    } else if (computerChoice == "Scissors") {
+        yourChoice.innerText = "Your choice: Paper " 
+        computerDecision.innerText = "Computer choice: " + computerChoice
+        decision.innerText = "You lose."
+        ++computerPoints 
+        computerScore.innerText = "Computer Score: " + computerPoints;
     }
 }
 
-function game() {        
-    if (scoreComputer === 5) {
-        return alert("You lost! Thanks for playing.")
+function choseScissors() {
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    if (computerChoice == "Scissors") {
+        yourChoice.innerText = "Your choice: Scissors " 
+        computerDecision.innerText = "Computer choice: " + computerChoice
+        decision.innerText = "It's a tie! Play again"
+    } else if (computerChoice == "Rock") {
+        yourChoice.innerText = "Your choice: Scissors " 
+        computerDecision.innerText = "Computer choice: " + computerChoice
+        decision.innerText = "You lose."
+        ++computerPoints 
+        computerScore.innerText = "Computer Score: " + computerPoints;
+    } else if (computerChoice == "Paper") {
+        yourChoice.innerText = "Your choice: Scissors " 
+        computerDecision.innerText = "Computer choice: " + computerChoice
+        decision.innerText = "You win."
+        ++playerPoints
+        playerScore.innerText = "Player Score: " + playerPoints;
     }
-    else if (scorePlayer === 5) {
-        return alert("You won! Thanks for playing.")
-    }
-    else {
-        oneRound();
-    }
+    console.log(playerPoints)
+
 }
 
-game();
+let endButton = document.createElement("button")
+        endButton.innerText = "Game Over. Play again?"
+        endButton.style.display = "none"
+        endButton.setAttribute("id", "endGame")
+        body.append(endButton);  
+
+endButton.addEventListener("click", startOver)
+
+function startOver() {
+    playerPoints = 0;
+    computerPoints = 0;
+    playerScore.innerText = "Player Score: " + playerPoints;
+    computerScore.innerText = "Computer Score: " + computerPoints;
+}
+ 
+
+
+
+// function startgame() {
+//     console.log("Hello")
+// };
